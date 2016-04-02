77e3a8793c28c7a838c458fb9d7b29e3e6b29756
loren <neutronstar@gmail.com>
2016-04-02 10:21:11 -0700

    going to change things

--- scrape.go
+++ scrape.go
@@ -2,9 +2,9 @@ package main
 
 import (
   "net/http"
-//  "net"
-//  "bufio"
   "os/exec"
+//  "os/signal"
+//  "syscall"
   "os"
   "fmt"
   "strings"
@@ -20,6 +20,56 @@ var current_scraper_port = 5000
 type Scraper struct {
   uri string
   cmd *exec.Cmd
+}
+
+type ScraperQueue struct {
+  scrapers []Scraper
+  current int
+}
+
+func NewScraperQueue() *ScraperQueue {
+  scrapers := make([]Scraper, num_scraper_proc)
+
+  for i := range scrapers {
+    s := *NewScraper()
+    s.cmd.Stdout = os.Stdout
+    s.cmd.Stderr = os.Stderr
+    go s.cmd.Run()
+    scrapers[i] = s
+  }
+
+//  stopSig := make(chan os.Signal, 1)
+//
+//  signal.Notify(
+//    stopSig,
+//    syscall.SIGHUP,
+//    syscall.SIGINT,
+//    syscall.SIGTERM,
+//    syscall.SIGQUIT,
+//  )
+//
+//  go func() {
+//    <-stopSig
+//    fmt.Println("RECVD SIGNAL")
+//    for _, s := range scrapers {
+//      s.cmd.Process.Kill()
+//    }
+//    os.Exit(0)
+//  }()
+
+
+  return &ScraperQueue{scrapers, 0}
+}
+
+func (q *ScraperQueue) Scrape(path string) error {
+  q.current %= num_scraper_proc
+
+  fmt.Println(q.scrapers[q.current].uri)
+  err := q.scrapers[q.current].Scrape(path)
+
+  q.current++
+
+  return err
 }
 
 func uri(host string, port int) string {
@@ -38,22 +88,13 @@ func NewScraper() *Scraper {
 
 func (s *Scraper) Scrape(path string) error {
   buffer := strings.NewReader(path)
-  _, error := http.Post(s.uri, "application/json", buffer)
-  return error
+  _, err := http.Post(s.uri, "application/json", buffer)
+  return err
 }
 
 func main() {
-  s := NewScraper()
-  fmt.Println(program)
-  fmt.Println(s.uri)
-  s.cmd.Stdout = os.Stdout
-  s.cmd.Stderr = os.Stderr
-  go s.cmd.Run()
-  s.Scrape("http://www.audiosf.com/events/")
-  s.Scrape("http://www.audiosf.com/events/")
-  s.Scrape("http://www.audiosf.com/events/")
-  s.Scrape("http://www.audiosf.com/events/")
-  s.Scrape("http://www.audiosf.com/events/")
-  s.Scrape("http://www.audiosf.com/events/")
-  s.cmd.Wait()
+  q := NewScraperQueue()
+  q.Scrape("http://www.audiosf.com/events/")
+  q.scrapers[0].cmd.Wait()
+  //s.cmd.Wait()
 }
